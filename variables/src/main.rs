e0449cfe7ad2160e699e08f640706d3f09f18d79
haoran <jinkun.hjk@antfin.com>
2020-07-13 12:41:39 +0800

    add data_types functions variables

---
+++ variables/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    /**
+    3.1. Variables and Mutability
+    */
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let mut spaces = " ";
+    let mut spaces = spaces.len();
+
+}
